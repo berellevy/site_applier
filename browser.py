d0e05a9bf27b633fc908c714f9c4e8fb3c9fc187
berellevy <berellevy@gmail.com>
2024-05-23 14:43:27 -0400

    initial commit

---
+++ browser.py
@@ -0,0 +1,16 @@
+import typing
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+
+
+def get_browser() -> webdriver.Chrome:
+    return webdriver.Chrome()
+
+
+
+
