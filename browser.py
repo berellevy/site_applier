d7c31900418731ddb36d1dc5448c1db855c74703
berellevy <berellevy@gmail.com>
2024-05-24 12:22:46 -0400

    good progress on workday

--- browser.py
+++ browser.py
@@ -2,14 +2,21 @@ import typing
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 
+from drop_files import drop_files
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
+CSS = By.CSS_SELECTOR
+XP = By.XPATH
 
 
 def get_browser() -> webdriver.Chrome:
--- workday.py
+++ workday.py
@@ -1,5 +1,7 @@
+from functools import cached_property
 from time import sleep
-from browser import webdriver, By, Keys
+from browser import webdriver, By, Keys, get_browser, ActionChains, EC, drop_files
+from selenium.webdriver.support.ui import WebDriverWait
 import os
 
 USERNAME = os.getenv("WORKDAY_USERNAME")
@@ -8,8 +10,19 @@ PASSWORD = os.getenv("WORKDAY_PASSWORD")
 CSS = By.CSS_SELECTOR
 XP = By.XPATH
 
+class ApplierError(BaseException):
+    pass
+
 def el_text_content(element, text):
     return f"//{element}[contains( text( ), '{text}')]"
+
+def data_id_find(el, data_id):
+    return (CSS, f"{el}[data-automation-id='{data_id}']")
+
+def find_field_by_label_content(browser, content):
+    field_id = browser.find_element(XP, el_text_content("*", content)).get_attribute("for")
+    return browser.find_element(CSS, f"#{field_id}")
+
 
 def text_input(browser, data_id, value):
     try:
@@ -31,22 +44,30 @@ def click_button(
     
     
 def how_did_you_hear_about_us(browser: webdriver.Chrome):
+    section_data_id = "sourceSection"
+    menu_open_data_id = "multiselectInputContainer"
+    submenu_button_selector = "div[aria-label='Job Board']"
+    selection_selector = "div[data-automation-label='LinkedIn.com']"
     try:
-        section = browser.find_element(CSS, "div[data-automation-id='sourceSection']")
-        section.find_element(CSS, "span[data-automation-id='promptIcon']").click()
+        section = browser.find_element(*data_id_find("div", section_data_id))
+
+        section.find_element(*data_id_find("div", menu_open_data_id)).click()
         sleep(1)
         popup = browser.find_element(CSS, "div[data-automation-widget='wd-popup']")
-        popup.find_element(CSS, "div[aria-label='Job Board']").click()
+        popup.find_element(CSS, submenu_button_selector).click()
         sleep(1)
-        popup.find_element(CSS, "div[data-automation-label='LinkedIn.com']").click()
+        popup.find_element(CSS, selection_selector).click()
 
     except Exception as e:
         print("how_did_you_hear_about_us", e, e.args)
 
 def have_you_ever_been_employed(browser):
+    sleep(.5)
     try:
         legend = browser.find_element(XP, el_text_content("legend", "Have you ever been employed"))
-        legend.parent.find_element(XP, el_text_content("label", "Yes")).click()
+        radio = legend.parent.find_element(XP, el_text_content("label", "Yes"))
+        radio.click()
+        radio.click()
     except Exception as e:
         print("have_you_ever_been_employed", e, e.args)
 
@@ -54,40 +75,119 @@ def have_you_ever_been_employed(browser):
 
 def simple_dropdown(browser, button_data_id, value):
     try:
-        browser.find_element(CSS, f"button[data-automation-id='{button_data_id}']").click()
+        button = browser.find_element(*data_id_find("button", button_data_id))
+        if button.text == value:
+            return
+        button.find_element(XP, "..").click()
         sleep(.5)
         popup = browser.find_element(CSS, "div[data-automation-widget='wd-popup']")
         popup.find_element(XP, el_text_content("div", value)).click()
     except Exception as e:
         print("simple_dropdown", button_data_id, e, e.args)
+
+
+def wait_for_element(browser, by, value, timeout = 10, raise_error = False):
+    try:
+        return (
+            WebDriverWait(browser,timeout,)
+            .until(EC.presence_of_element_located((by, value)))
+        )
+    except Exception as e:
+        if raise_error:
+            raise e
+        else:
+            return False
+
+
+class Applier:
+    def __init__(self, url, browser = None) -> None:
+        """"""
+        self.url = url
+        self.browser = browser or get_browser()
+        self.action_chain = ActionChains(self.browser)
     
 
 
-def apply(link: str, browser, action_chain):
-    browser.get(link)
-    # LOGIN
-    sleep(3)
-    text_input(browser, "email", USERNAME,)
-    text_input(browser, "password", PASSWORD,)
-    sleep(.5)
-    click_button(browser, CSS, "div[data-automation-id='click_filter']")
-
-
-    how_did_you_hear_about_us(browser)
-    have_you_ever_been_employed(browser)
-    simple_dropdown(browser, "countryDropdown", "United States of America")
-
-    text_input(browser, "legalNameSection_firstName", "Dovber", )
-    text_input(browser, "legalNameSection_lastName", "Levy", )
-
-    text_input(browser, "addressSection_addressLine1", "575 East ny", )
-    text_input(browser, "addressSection_city", "brooklyn", )
-    simple_dropdown(browser, "addressSection_countryRegion", "New York")
-    text_input(browser, "addressSection_postalCode", "11225", )
-    text_input(browser, "phone-number", "9176798518", )
-
-
-    return browser
+    def go(self):
+        self.browser.get(self.url)
 
 
 
+    def wait_for_page_load(self, data_id):
+        return wait_for_element(
+            self.browser, 
+            CSS, f"div[data-automation-id='{data_id}']", 
+            raise_error=True
+        )
+    
+    def check_for_login(self):
+        return wait_for_element(self.browser, *data_id_find("div", "signInContent"))
+
+    def fill_login(self):
+        print("login")
+        text_input(self.browser, "email", USERNAME,)
+        text_input(self.browser, "password", PASSWORD,)
+        wait_for_element(
+            self.browser,
+            CSS,"div[role='button'][aria-label='Sign In']",
+        ).click()
+
+    def check_for_contact_info_page(self):
+        return wait_for_element(self.browser, *data_id_find("div", "contactInformationPage"))
+    
+    def fill_contact_info(self):
+        how_did_you_hear_about_us(self.browser)
+        sleep(.5)
+        have_you_ever_been_employed(self.browser)
+        sleep(.5)
+        simple_dropdown(self.browser, "countryDropdown", "United States of America")
+        sleep(.5)
+        text_input(self.browser, "legalNameSection_firstName", "Dovber")
+        sleep(.5)
+        text_input(self.browser, "legalNameSection_lastName", "Levy")
+        sleep(.5)
+        simple_dropdown(self.browser, "phone-device-type", "Mobile")
+        sleep(.5)
+        text_input(self.browser, "phone-number", "9176798518", )
+        sleep(.5)
+        self.browser.find_element(*data_id_find("button","bottom-navigation-next-button")).click()
+        sleep(4)
+        if wait_for_element(self.browser, *data_id_find("div", "contactInformationPage"), timeout=1):
+            raise ApplierError("still on contact info page")
+
+    def check_for_experience_page(self):
+        return wait_for_element(self.browser, *data_id_find("div", "myExperiencePage"), raise_error=True)
+
+    def fill_experience(self):
+        resume_path = os.getcwd() + r"\BerelLevy_Resume.pdf"
+        resume_section = self.browser.find_element(*data_id_find("div", "resumeUpload"))
+        drop_zone = resume_section.find_element(*data_id_find("div", "file-upload-drop-zone"))
+        drop_files(drop_zone, resume_path)
+        self.browser.find_element(*data_id_find("button","bottom-navigation-next-button")).click()
+        sleep(4)
+        if wait_for_element(self.browser, *data_id_find("div", "myExperiencePage"), timeout=1):
+            raise ApplierError("still on myExperiencePage")
+        
+
+    def check_for_questions_page(self):
+        return wait_for_element(
+            self.browser,
+            *data_id_find("div", "primaryQuestionnairePage")
+        )
+
+    # def fill_questions_page(self):
+
+
+    def run(self):
+        self.go()
+        if self.check_for_login():
+            sleep(.4)
+            self.fill_login()
+        if self.check_for_contact_info_page():
+            self.fill_contact_info()
+        if self.check_for_experience_page():
+            self.fill_experience()
+
+
+        
+    
