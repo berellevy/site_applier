7673c9fe5e7e5f5bf7c24d5381657adabbcc37bb
berellevy <berellevy@gmail.com>
2024-06-07 12:02:47 -0400

    move answers into parent section. add key_is_prefix lookup

--- form_fields/base_form_field.py
+++ form_fields/base_form_field.py
@@ -1,13 +1,23 @@
 from browser import D, XP, find_element
 
+def key_is_prefix(d: dict, lookup: str):
+  """
+  Access a key in a dict even if the key only matches part of the lookup prefix.
+  Not efficient at all. Only good for small dicts. (oh baby)
+  """
+  for key in (d):
+    if lookup.startswith(key):
+      return d[key]
+
 
 class BaseFormField:
   XPATH: str
   NAME_XPATH: str
 
   @classmethod
-  def find_all(cls, browser: D, parent_section):
-    return [cls(f, parent_section) for f in browser.find_elements(XP, cls.XPATH)]
+  def find_all(cls, browser: D, parent_section, custom_xpath: str = None):
+    xpath = custom_xpath or cls.XPATH
+    return [cls(f, parent_section) for f in browser.find_elements(XP, xpath)]
 
   def __init__(self, element: D, parent_section) -> None:
     self.element = element
@@ -16,6 +26,22 @@ class BaseFormField:
   @property
   def name(self) -> str:
     return find_element(self.element, XP, self.NAME_XPATH).text  
+  
+  @property
+  def correct_answer(self) -> str:
+    """
+    if an exact match isn't found, it then searches for an answer whos key
+    matches the beginning of the field name.
+    Example: 
+    The field name "Have you worked at Etsy, Reverb, or Depop before?*"
+    will match the answer key "Have you worked at"
+    """
+    answers = self.parent_section.ANSWERS.get((type(self)))
+    return answers.get(self.name) or key_is_prefix(answers, self.name)
+  
+  @property
+  def path(self) -> list[str]:
+    return [*self.parent_section.path, self.name]
 
   @property
   def is_required(self) -> bool:
