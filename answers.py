47e992e82092dc14ce4619c55bd1dd980e75fc3a
berellevy <berellevy@gmail.com>
2024-06-17 17:37:27 -0400

    add func get_sub_sections

--- answers.py
+++ answers.py
@@ -1,36 +1,53 @@
 import pandas as pd
 
-_df = pd.read_json("answers.json")
+
+def load_answers() -> pd.DataFrame:
+  return pd.concat([
+    pd.DataFrame(columns=["page", "section", "sub_section", "field_type", "field_name", "answer"], dtype=object),
+    pd.read_json("answers.json"),
+
+  ])
+_df = load_answers()
+
+
+def get_sub_sections(*, page, section):
+  return list(
+    _df.loc[
+      (_df["page"] == "My Experience") &
+      (_df["section"] == "Work Experience"),
+      "sub_section"
+    ]
+    .unique()
+  )
+  
 
 
 def get(
-    page: str = None,
-    field_type: str = None,
-    field_name: str = None,
-    section: str = None,
-    sub_section: str = None,
+  page: str = None,
+  field_type: str = None,
+  field_name: str = None,
+  section: str = None,
+  sub_section: str = None,
 ):
 
-    base_filters = (
-        (_df["page"].eq(page) if page else True) &
-        (_df["field_type"].eq(field_type) if field_type else True) &
-        (_df["section"].eq(section) if section else True) &
-        (_df["sub_section"].eq(sub_section) if sub_section else True) 
-    )
+  base_filters = (
+    (_df["page"].eq(page) if page else True) &
+    (_df["field_type"].eq(field_type) if field_type else True) &
+    (_df["section"].eq(section) if section else True) &
+    (_df["sub_section"].eq(sub_section) if sub_section else True) 
+  )
 
-    exact_match = _df.loc[
-        base_filters &
-        (_df["field_name"].eq(field_name) if field_name else True)
-        ,
-        "answer"
+  answer = _df.loc[
+    base_filters &
+    (_df["field_name"].eq(field_name) if field_name else True)
+    ,
+    "answer"
+  ]
+  if answer.empty:
+    answer = _df.loc[
+      base_filters &
+      (_df["field_name"].apply(lambda x: field_name.startswith(x)) if field_name else True)
+      ,
+      "answer"
     ]
-    if exact_match.empty:
-        print(field_name)
-        startswith = _df.loc[
-            base_filters &
-            (_df["field_name"].apply(lambda x: field_name.startswith(x)) if field_name else True)
-            ,
-            "answer"
-        ]
-        return list(startswith)
-    return list(exact_match)
+  return next(iter(answer.values), None)
