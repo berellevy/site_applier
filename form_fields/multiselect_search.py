a07a18f18ed8a4cd510b41a99cfd1842bbfc30e6
berellevy <berellevy@gmail.com>
2024-06-18 17:38:04 -0400

    make it more efficient and work out kinks

--- form_fields/multiselect_search.py
+++ form_fields/multiselect_search.py
@@ -40,28 +40,36 @@ class MultiselectSearchField(BaseFormField):
   
   def open_dropdown(self):
     if not self.dropdown_open:
-      el = find_element(self.element, *data_id_find("div", "multiselectInputContainer"))
+      el = find_element(
+        self.element, 
+        XP,
+        """.//span[
+        @data-automation-id='promptIcon' or @data-automation-id='promptSearchButton'
+        ]"""
+      )
+      # el = find_element(self.element, *data_id_find("div", "multiselectInputContainer"))
       move_to_element(el, extra_up=-200)
       el.click()
   
-  def remove_answers(self):
-    while len(self.answer_elements):
-      for answer_element in self.answer_elements:
+
+  def remove_incorrect_answers(self):
+    correct_answers = set(self.correct_answer)
+    for answer_element in self.answer_elements:
+      if not answer_element.text in correct_answers:
         delete_button_element = find_element(answer_element, *data_id_find("div", "DELETE_charm"))
         move_to_element(self.element, extra_up=-200)
         delete_button_element.click()
         sleep(.1)
 
-
   def fill_one(self, answer: str, search_element = None):
-
+    # EXECUTE SEARCH FOR ANSWER
     search_element = search_element or find_element(self.element, *data_id_find("input", "searchBox"))
     move_to_element(search_element, extra_up=-200)
     search_element.send_keys(SELECT_ALL)
     search_element.send_keys(answer)
     search_element.send_keys(Keys.ENTER)
+    # SELECT ANSWER WHEN IT LOADS
     choice_element = wait_for_element(self.dropdown_element, XP, f".//div[@aria-label='{answer}']", timeout=3)
-
     if choice_element and (not find_element(choice_element, XP, ".//div[@data-automation-checked='Checked']")):
       move_to_element(choice_element, extra_up=-200)
       choice_element.click()
@@ -70,11 +78,12 @@ class MultiselectSearchField(BaseFormField):
   
   
   def _fill(self):
-    self.remove_answers()
+    self.remove_incorrect_answers()
     self.open_dropdown()
     search_element = find_element(self.element, *data_id_find("input", "searchBox"))
-    for answer in self.correct_answer:
-      self.fill_one(answer, search_element)
-      sleep(.5)
+    for correct_answer in self.correct_answer:
+      if not correct_answer in self.answer:
+        self.fill_one(correct_answer, search_element)
+        sleep(.5)
     move_to_element(search_element, extra_up=-200)
     search_element.click()
