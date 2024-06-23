98a2e619081203b88bceb6e46f19fd3e0277250a
berellevy <berellevy@gmail.com>
2024-06-23 17:47:49 -0400

    reorganize utils. add default next page button. upgrade xpaths.

--- pages.py
+++ pages.py
@@ -3,13 +3,12 @@ A class for each page.
 """
 
 import os
-from browser import D, XP, find_element, data_id_find, remove_element, xp_attr_starts_with
-from form_fields import BaseFormField, TextInput, MultiselectSearchField, Dropdown, Radio
-from form_fields.single_checkbox import SingleCheckBox
-from form_fields.text_area import TextArea
-from form_fields.file_upload import MultiFileUpload
-from form_fields.multi_section import MultiSection
-import xpaths
+from utils import D, find_element, remove_element, d_id, xpaths
+# from form_fields import BaseFormField, TextInput, MultiselectSearchField, Dropdown, Radio
+# from form_fields.single_checkbox import SingleCheckBox
+# from form_fields.text_area import TextArea
+# from form_fields.file_upload import MultiFileUpload
+from form_fields import get_fields, BaseFormField
 
 
 
@@ -17,28 +16,19 @@ import xpaths
 class BasePage:
   name: str
   XPATH: str = ""
-  NEXT_PAGE_BUTTON_XPATH: str = ""
+  NEXT_PAGE_BUTTON_XPATH: str = f"//button[{d_id('bottom-navigation-next-button')}]"
   def __init__(self, browser: D) -> None:
     self.browser = browser
   
   @classmethod
   def is_current(cls, browser: D, custom_xpath: str | None = None) -> bool:
     xpath = custom_xpath or cls.XPATH
-    if find_element(browser, XP, cls.XPATH):
+    if find_element(browser, xpath):
       return cls(browser)
 
   @property
   def fields(self) -> BaseFormField:
-    return [
-      *TextInput.find_all(self.browser, self),
-      *MultiselectSearchField.find_all(self.browser, self),
-      *Dropdown.find_all(self.browser, self),
-      *Radio.find_all(self.browser, self),
-      *MultiSection.find_all(self.browser, self),
-      *MultiFileUpload.find_all(self.browser, self),
-      *TextArea.find_all(self.browser, self),
-      *SingleCheckBox.find_all(self.browser, self),
-    ]
+    return get_fields(self)
   
   @property
   def path(self) -> list[str]:
@@ -47,7 +37,7 @@ class BasePage:
   
   def delete_header(self):
     """The header element sometimes blocks clicks to form fields."""
-    if header:=find_element(self.browser, *data_id_find("div", "header")):
+    if header:=find_element(self.browser, f".//div[{d_id('header')}]"):
       remove_element(header)
 
   def fill(self):
@@ -56,7 +46,7 @@ class BasePage:
 
   def next_page(self):
     self.fill()
-    find_element(self.browser, XP, self.NEXT_PAGE_BUTTON_XPATH).click()
+    find_element(self.browser, self.NEXT_PAGE_BUTTON_XPATH).click()
 
 
 PAGES: list[BasePage] = []
@@ -88,7 +78,7 @@ class StartApplication(BasePage):
   """this is a little popup with a choice of how to fill out the app."""
   name = "Start Your Application"
   XPATH = xpaths.START_APPLICATION_PAGE
-  NEXT_PAGE_BUTTON_XPATH = "//a[@data-automation-id='useMyLastApplication']"
+  NEXT_PAGE_BUTTON_XPATH = f"//a[{d_id('useMyLastApplication')}]"
   
 
 
@@ -97,45 +87,38 @@ class StartApplication(BasePage):
 class SignIn(BasePage):
   name = "Sign In"
   XPATH = xpaths.SIGN_IN_PAGE
-  NEXT_PAGE_BUTTON_XPATH = """
-  //button[@data-automation-id='signInSubmitButton']/parent::div
-  """
+  NEXT_PAGE_BUTTON_XPATH = f"//button[{d_id('signInSubmitButton')}]/parent::div"
   
   
 @register
 class MyInformation(BasePage):
   name = "My Information"
   XPATH = xpaths.MY_INFORMATION_PAGE
-  NEXT_PAGE_BUTTON_XPATH = "//button[@data-automation-id='bottom-navigation-next-button']"
+
   def fill(self):
     self.delete_header()
     super().fill()
-
 
 
 @register
 class MyExperience(BasePage):
   name = "My Experience"
   XPATH = xpaths.MY_EXPERIENCE_PAGE
-  NEXT_PAGE_BUTTON_XPATH = "//button[@data-automation-id='bottom-navigation-next-button']"
     
 @register
 class ApplicationQuestions(BasePage):
   name = "Application Questions"
   XPATH = xpaths.APPLICATION_QUESTIONS_PAGE
-  NEXT_PAGE_BUTTON_XPATH = "//button[@data-automation-id='bottom-navigation-next-button']"
 
 @register
 class VoluntaryDisclosures(BasePage):
   name = "Voluntary Disclosures"
   XPATH = xpaths.VOLUNTARY_DISCLOSURES_PAGE
-  NEXT_PAGE_BUTTON_XPATH = "//button[@data-automation-id='bottom-navigation-next-button']"
 
 @register
 class Review(BasePage):
   name = "Review"
   XPATH = xpaths.REVIEW_PAGE
-  NEXT_PAGE_BUTTON_XPATH = "//button[@data-automation-id='bottom-navigation-next-button']"
 
 
 @register
