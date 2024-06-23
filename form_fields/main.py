5997b3cb19ba766bb1565adbdce38401011e5181
berellevy <berellevy@gmail.com>
2024-06-23 17:52:09 -0400

    create get_fields method

--- form_fields/main.py
+++ form_fields/main.py
@@ -1,3 +1,5 @@
+from .single_checkbox import SingleCheckBox
+from .text_area import TextArea
 from .dropdown import Dropdown
 from .multiselect_search import MultiselectSearchField
 from .radio import Radio
@@ -5,3 +7,15 @@ from .text_input import TextInput
 from .base_form_field import BaseFormField
 from .multi_section import MultiSection
 from .file_upload import MultiFileUpload
+
+def get_fields(page) -> BaseFormField:
+  return [
+    *TextInput.find_all(page.browser, page),
+    *MultiselectSearchField.find_all(page.browser, page),
+    *Dropdown.find_all(page.browser, page),
+    *Radio.find_all(page.browser, page),
+    *MultiSection.find_all(page.browser, page),
+    *MultiFileUpload.find_all(page.browser, page),
+    *TextArea.find_all(page.browser, page),
+    *SingleCheckBox.find_all(page.browser, page),
+  ]
